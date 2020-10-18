4f7fe52cac14d01376b66df2435bfd86c0ce0470
Dungchv13 <Dungutc13@gmail.com>
2020-10-18 10:41:13 +0700

    [O]_thuchanh1

---
+++ src/MyQueue.java
@@ -0,0 +1,50 @@
+public class MyQueue {
+    private int capacity;
+    private int[] queueArr;
+    private int head;
+    private int tail;
+    private int currentSize = 0;
+
+    public MyQueue(int queueSize){
+        capacity = queueSize;
+        queueArr = new int[capacity];
+    }
+
+    public boolean isQueueFull(){
+        if(currentSize == capacity){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isQueueEmpty(){
+        if(currentSize == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public void enqueue(int num){
+        if(isQueueFull()){
+            System.out.println("Queue is full");
+            return;
+        }
+        queueArr[currentSize] = num;
+        currentSize++;
+        System.out.println(num+" have been add to Queue");
+    }
+
+    public void dequeue(){
+        if(isQueueEmpty()){
+            System.out.println("Queue is Empty");
+            return;
+        }
+        for (int i = 0; i < currentSize - 1; i++) {
+            queueArr[i] = queueArr[i+1];
+        }
+        queueArr[currentSize] = 0;
+        currentSize--;
+        System.out.println(queueArr[0]+" have been remove from Queue");
+
+    }
+}
